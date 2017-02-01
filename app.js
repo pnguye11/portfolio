6e4abc671dade456c4c984c391ad296da4959f0e
Phuong Nguyen <pnguye11@gmail.com>
2017-02-01 12:27:18 -0800

    update index.html

--- app.js
+++ app.js
@@ -7,21 +7,21 @@ app.config(function ($stateProvider, $urlRouterProvider) {
     .state('home', {
         url: '/home',
         controller: 'homeController as home',
-        templateUrl: '/partials/home.html'
+        templateUrl: 'partials/home.html'
     })
     .state('about', {
         url: '/about',
         controller: 'aboutController as about',
-        templateUrl: '/partials/about.html'
+        templateUrl: 'partials/about.html'
     })
     .state('projects', {
         url: '/projects',
         controller: 'projectsController as projects',
-        templateUrl: '/partials/projects.html'
+        templateUrl: 'partials/projects.html'
     })
     .state('contact', {
         url: '/contact',
         controller: 'contactController as contact',
-        templateUrl: '/partials/contact.html'
+        templateUrl: 'partials/contact.html'
     })
   })
